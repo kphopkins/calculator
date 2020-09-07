c21cc2e8690fb7fe9fab450deb177ba867727d4f
kristen <pumikinforhire@gmail.com>
2020-09-06 21:37:21 -0500

    Delete button fully functional

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let num = "";
 
 const display = document.querySelector("#display");
 
+let lastClicked = "";
+
 const numBtn = document.querySelectorAll(".num");
 numBtn.forEach(button => button.addEventListener("click", () => {
     if (operators[0] === "=") {
@@ -11,9 +13,15 @@ numBtn.forEach(button => button.addEventListener("click", () => {
         num = button.innerHTML;
         display.innerHTML = `${button.innerHTML}`;
     } else {
-        display.innerHTML += `${button.innerHTML}`;
-        num += button.innerHTML;
+        if (display.innerHTML.includes(".") && button.innerHTML == ".") {
+            return;
+        } else {
+            display.innerHTML += `${button.innerHTML}`;
+            num += button.innerHTML;
+        }
     }
+
+    lastClicked = "num";
 }));
 
 const opBtn = document.querySelectorAll(".op");
@@ -26,8 +34,10 @@ opBtn.forEach(button => button.addEventListener("click", () => {
     operators.push(button.innerHTML);
     num = "";
 
-    display.innerHTML += ` ${button.innerHTML} `;
+    display.innerHTML += `${button.innerHTML}`;
     console.table(numbers);
+
+    lastClicked = "op";
 }));
 
 const equalsBtn = document.querySelector("#equals");
@@ -38,11 +48,16 @@ equalsBtn.addEventListener("click", () => {
         numbers.push(Number(num));
         
         let result = Math.round(numbers.reduce(operate) * 100) / 100;
-        display.innerHTML = result;
+        
+        num = result.toString();
+        if (num == "NaN") {
+            display.innerHTML = "Whoops!<br> Computers can't divide by 0.";
+        } else {
+            display.innerHTML = result;
+        }
         
         numbers = [];
-        num = result.toString();
-
+        lastClicked = "";
         operators.push("=");
     }
 });
@@ -54,8 +69,24 @@ function clearCalc() {
     operators = [];
     numbers = [];
     num = "";
+    lastClicked = "";
     display.innerHTML = "";
 }
+
+const delBtn = document.querySelector("#del");
+delBtn.addEventListener("click", () => {
+    let text = display.innerHTML.split("");
+    text.pop();
+    display.innerHTML = text.join("");
+
+    if (lastClicked = "num") {
+        let numList = num.split("");
+        numList.pop();
+        num = numList.join("");
+    } else if (lastClicked = "op") {
+        operators.pop();
+    }
+})
 
 function operate (a, b) {
     let op = operators[0];
@@ -88,5 +119,8 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-    return a / b;
+    if (a / b == "Infinity" || a / b == "-Infinity") {
+    } else {
+        return a / b; 
+    }
 }
